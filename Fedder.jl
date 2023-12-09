a0d34480a80122d086fa13856d0775090ab9f3aa
jjgarzella <jjgarzella@gmail.com>
2023-12-09 15:00:31 -0500

    Add new files
    Add an experiments file and one to
    put varios Fedder-type criteria

---
+++ Fedder.jl
@@ -0,0 +1,20 @@
+
+"""
+Returns true if the hypersurface
+defined by the homogeneous poynomial f
+is F-split in characteristic p
+"""
+function fedder(p,f)
+  fpower = f^(p-1); 
+
+end
+
+function delta1(p,a)
+  # code here
+end
+
+
+function quasiFedder(p,f)
+  
+end
+
